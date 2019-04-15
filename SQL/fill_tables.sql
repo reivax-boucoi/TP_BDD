88968737d0de3ea252466600983017392ae60919
reivax-boucoi <reivax.boucoi@laposte.net>
2019-04-15 11:38:07 +0200

    Update fill_tables.sql
    J'aime les kiwis.

--- SQL/fill_tables.sql
+++ SQL/fill_tables.sql
@@ -15,47 +15,47 @@ VALUES ('Einstein', 'Albert', '12 Aenue de Zurich', '1801-10-19', '1819-06-14', 
 
 
 INSERT INTO `Scientifique` (`idScientifique`, `Grade`)
-VALUES (1,3)
+VALUES (1,3);
 INSERT INTO `Scientifique` (`idScientifique`, `Grade`)
-VALUES (118518,2)
+VALUES (118518,2);
 INSERT INTO `Scientifique` (`idScientifique`, `Grade`)
-VALUES (10,1)
+VALUES (10,1);
 INSERT INTO `Scientifique` (`idScientifique`, `Grade`)
-VALUES (007,3)
+VALUES (007,3);
 INSERT INTO `Scientifique` (`idScientifique`, `Grade`)
-VALUES (999,1)
+VALUES (999,1);
 
 INSERT INTO `Doctorant` (`idDoctorant`, `date_soutenance`)
-VALUES (121314,1938-12-01)
+VALUES (121314,'1938-12-01');
 INSERT INTO `Doctorant` (`idDoctorant`, `date_soutenance`)
-VALUES (151617,2012-12-25)
+VALUES (151617,'2012-12-25');
 INSERT INTO `Doctorant` (`idDoctorant`, `date_soutenance`)
-VALUES (1,2019-04-10)
+VALUES (1,'2019-04-10');
 
 INSERT INTO `ScientifiqueEncadreDoctorant` (`idScientifique`,`idDoctorant`)
-VALUES (118518, 151617)
+VALUES (118518, 151617);
 INSERT INTO `ScientifiqueEncadreDoctorant` (`idScientifique`,`idDoctorant`)
-VALUES (10, 121314)
+VALUES (10, 121314);
 INSERT INTO `ScientifiqueEncadreDoctorant` (`idScientifique`,`idDoctorant`)
-VALUES (10, 1)
-
-INSERT INTO `PersonnelParticipeJPO` (`idPersonnel`, `idJPO`)
-VALUES (1, 12)
-INSERT INTO `PersonnelParticipeJPO` (`idPersonnel`, `idJPO`)
-VALUES (118518, 13)
+VALUES (10, 1);
 
 INSERT INTO `JPO` (`idJPO`,`date_debut`,`date_fin`)
-VALUES (12,'2019-03-25','2019-03-27')
+VALUES (12,'2019-03-25','2019-03-27');
 INSERT INTO `JPO` (`idJPO`,`date_debut`,`date_fin`)
-VALUES (13,'2015-04-12','2015-04-14')
+VALUES (13,'2015-04-12','2015-04-14');
+
+INSERT INTO `PersonnelParticipeJPO` (`idPersonnel`, `idJPO`)
+VALUES (1, 12);
+INSERT INTO `PersonnelParticipeJPO` (`idPersonnel`, `idJPO`)
+VALUES (118518, 13);
 
 INSERT INTO `Etablissement` (`idEtablissement`,`Nom`,`Acronyme`,`Adresse`)
-VALUES (51,'Entreprise Décision Financières','EDF','48 impasse du budget')
+VALUES (51,'Entreprise Décision Financières','EDF','48 impasse du budget');
 INSERT INTO `Etablissement` (`idEtablissement`,`Nom`,`Acronyme`,`Adresse`)
-VALUES (52,'H2C corporation','H2Ccorp','62 rue de l'eauc')
+VALUES (52,'H2C corporation','H2Ccorp','62 rue de l'eauc');
 
 INSERT INTO `Enseignant_chercheur` (`Echelon`,`idEtablissement`,`idScientifique`)
-VALUES (2,51,10)
+VALUES (2,51,10);
 
 INSERT INTO `Conference` (`idConference`,`date_debut`,`date_fin`,`idPresident`)
 VALUES (
